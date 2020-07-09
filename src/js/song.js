fd49dd6087335f8a29620c8edb946036eb31e66d
shirley <366603960@qq.com>
2020-07-09 10:26:35 +0800

    add song page

--- src/js/song.js
+++ src/js/song.js
@@ -1,50 +1,27 @@
-
-  require.config({
-    paths: {
-      "jquery": "../lib/jquery-3.5.1"
-    }
-  });
-
-  define(['jquery'], function($){
-    $.ajax({
-        url : 'http://localhost:3000/artist/top/song?id=12429072' ,
-        success(data){
-            var songs  = data.songs;
-            let arr = songs.map(function(ele,i){
-                // console.log(ele.al.picUrl) //singer's picture
-                return `<tr>
-                            <td width="50px">${i+1}</td>
-                            <td width="20px"><i class="iconfont icon-start"></i></td>
-                            <td>${ele.name}</td>//song's name
-                            <td>歌曲时长</td>
-                            <td>${ele.al.name}</td>//album ' name
-                        </tr>`
-            }).join('');
-            $(arr).appendTo( '.singer-list');
-        },
-        error(err){  
-          console.log("err");
-      }
-  })
-  
-  //hot singer
-  $.ajax({
-    url : 'http://localhost:3000/top/artists' ,
-    success(data){
-        var hotSinger  = data.artists;
-        hotSinger.length = 6;
-        let singer = hotSinger.map(function(ele,i){
-            return `<div>
-                      <img src=${ele.picUrl} >
-                      <a>${ele.name}</a>
-                    </div>`;
-        }).join('');
-        $(singer).appendTo( '.singer-hot' );
-    },
-    error(err){  
-      console.log("err");
+require.config({
+  paths: {
+    "jquery": "../lib/jquery-3.5.1",
+    "api": "api"
   }
-})
+});
 
-    
+define(['jquery', 'api'], function($, api){
+
+  var str = location.search //?songId=2222222
+  var patt1 = /\?songId=([0-9]*)/;
+  var songId = str.match(patt1)[1];
+  console.log(songId);
+
+
+  //获取lyric数据并渲染
+  api.apiLyric("31311140").then(function(res){
+      if(res.code == "200"){
+        let lyric = res.lrc.lyric;
+        let lyricStr = `<div>${lyric}</div>`
+        $(lyricStr).appendTo( '.lyrics');
+      }  
   });
+
+
+  
+});
