c915cbc8aded8ad72bd23248c7444406682a9f15
shirley <366603960@qq.com>
2020-07-09 11:42:18 +0800

    add fold function

--- src/js/song.js
+++ src/js/song.js
@@ -5,23 +5,44 @@ require.config({
   }
 });
 
-define(['jquery', 'api'], function($, api){
+define(['jquery', 'api','lyric'], function($, api,Lyric){
 
-  var str = location.search //?songId=2222222
-  var patt1 = /\?songId=([0-9]*)/;
-  var songId = str.match(patt1)[1];
-  console.log(songId);
+  // var str = location.search //?songId=2222222
+  // var patt1 = /\?songId=([0-9]*)/;
+  // var songId = str.match(patt1)[1];
+  // console.log(songId);
 
 
   //获取lyric数据并渲染
   api.apiLyric("31311140").then(function(res){
       if(res.code == "200"){
         let lyric = res.lrc.lyric;
-        let lyricStr = `<div>${lyric}</div>`
-        $(lyricStr).appendTo( '.lyrics');
+        let lyricRes = new Lyric(lyric);
+        let lyricArr = lyricRes.toStrArr();
+        let lyricHtml = lyricArr.map(function(ele,i){
+            return `<span class="lyric">${ele}</span>
+                    `;
+        }).join('<br>') 
+        $( '.lyrics').append( $(lyricHtml) ) 
+        $( '.lyricsWrap').append( $(lyricHtml) ) 
       }  
   });
 
 
-  
+
+  $('.unfold').click(function(){
+    $( '.lyricsWrap').hide();
+    $( '.lyrics').show(); 
+    $( '.unfold').hide();   
+    $( '.fold').show();  
+  })
+
+  $('.fold').click(function(){
+    $( '.lyricsWrap').show();
+    $( '.lyrics').hide();  
+    $( '.unfold').show();   
+    $( '.fold').hide();  
+  })
+
+
 });
